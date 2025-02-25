9d48845e442ed09be142a2870d459ddf2ec53fd0
Jay Patel <jay.patel1@gameskraft.com>
2025-02-25 22:34:25 +0530

    higher order functions

---
+++ Namaste-Javascript/07-Higher-Order-function.js
@@ -0,0 +1,39 @@
+// what is Higher order function ?
+// A function which takes another function as an argument or returns a function is called HOF.
+
+function x() {
+  console.log("function x");
+}
+
+function y(x) {
+  x();
+  console.log("function y");
+}
+// in above example, y is HOF as it takes x as an argument while x is a callback function
+
+// Functional Programming using Higher order Functions
+function area(radius) {
+  return Math.PI * radius * radius;
+}
+
+function circumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+function diameter(radius){
+    return 2 * radius;
+}
+
+function calculate(arr, logic){
+    let output = [];
+    for( const num of arr){
+        output.push(logic(num));
+    }
+    return output;
+}
+
+const arr = [2,4,5,9];
+
+console.log(calculate(arr, area))
+console.log(calculate(arr, diameter))
+console.log(calculate(arr, circumference))
