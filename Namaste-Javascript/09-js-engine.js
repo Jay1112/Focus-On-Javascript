6288fea417f59157a1ced388309cbec369627cc4
Jay Patel <jay.patel1@gameskraft.com>
2025-03-04 22:14:20 +0530

    js engine

---
+++ Namaste-Javascript/09-js-engine.js
@@ -0,0 +1,45 @@
+// Javascript Runtime Environment
+// JS Runtime Environment is an environment which has all the necessary tools and things required to run javascript code.
+
+// JS Environment containS
+// [1]. JS Engine 
+// [2]. Event Loop
+// [3]. Callback Queue
+// [4]. Micro-Task Queue
+// [5]. APIs
+
+/* Javascript Engine Architecture */
+// JS Engine is not a machine, it is a code which is written in low level language.
+// Javascript engine takes the code and do the following thing :
+    // Parsing ->  Compilation -> Execution
+
+/* Parsing Phase */
+// in this Phase, code would be divided into Tokens.
+// then, Syntax Parser takes this code and convert it into AST( Abstract Syntax Tree )
+// then, this AST is passed to Compilation Phase.
+
+/* Compilation */
+// JIT ( Just In Time Compilation )
+
+// Interpretor
+// Interpretor executes code line by line and It is fast in exectuion as compared to Compiler.
+
+// Compiler 
+// Compiler first compiles the code and converts the code into optimized version of the code and then executes it.
+// Compiler is slow as compared to Interpretor because it first compiles the code and then run it.
+
+// Javascript can be interpretted language or can be compiled language, it depends on JS Engine.
+
+// In, Modern Browsers
+// JS is both interpreted and JIT compiled language.
+// First, Code is compiled into machine code at runtime and later executed.
+
+// Garbage Collector
+// Garbage collector tries to clean up space whenever possible from the heap by using Mark & Sweep Algorithm.
+
+
+
+
+
+
+
