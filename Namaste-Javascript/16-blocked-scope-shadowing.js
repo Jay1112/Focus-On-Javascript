da9d1b449d465066f2e5fcccc1800f3ae97ab99b
Jay Patel <jay.patel1@gameskraft.com>
2025-03-26 14:10:50 +0530

    blocked scope

---
+++ Namaste-Javascript/16-blocked-scope-shadowing.js
@@ -0,0 +1,15 @@
+// what is Block in JS
+// Block is compound statement.
+// Block is pair of curly braces {} used to group one or more statements.
+
+// what is Scope in JS
+// The scope is current context of execution in which 
+// values and expressions are visible or can be referred.
+
+// let and const follow Blocked Scope.
+
+// illegal shadowing
+// let a = 20;
+// {
+//     var a = 30;
+// }
