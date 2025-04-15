00796c4cd3b168a0868a8f42676526240a6be258
Jay Patel <jay.patel1@gameskraft.com>
2025-04-15 22:31:30 +0530

    defined vs not defined

---
+++ Namaste-Javascript/19-not-defined-vs-undefined.js
@@ -0,0 +1,9 @@
+// undefined
+
+// undefined in javascript means memory has been allocated to variable but 
+// value is not initialized yet.
+
+// not defined 
+// not defined means there is a no variable is declare and it does not have a memory.
+
+// loosely typed means there is a no any data type has been declared for a variable.
